cac86eac52040679e5b6c50695f92b48bdf8fc55
MaciekJozwicki <118013298+MaciekJozwicki@users.noreply.github.com>
2023-05-01 18:44:03 +0000

    Deploying to gh-pages from @ MaciekJozwicki/goit-js-hw-09@4e678a8438e5a9b5b554cda39811a776405c172f 🚀

---
+++ 01-color-switcher.9a88bdb8.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){t.disabled=!0})),e.addEventListener("click",(function(){e.disabled=!0})),console.log(t,e,n)}();
+//# sourceMappingURL=01-color-switcher.9a88bdb8.js.map
