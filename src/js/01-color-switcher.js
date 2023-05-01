4e678a8438e5a9b5b554cda39811a776405c172f
Maciek <maciekjozwicki@wp.pl>
2023-05-01 20:43:34 +0200

    update task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,18 @@
+const startBtnEl = document.querySelector('button[data-start]')
+const stopBtnEl = document.querySelector('button[data-stop]')
+const bodyEl = document.querySelector('body')
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
+startBtnEl.addEventListener("click", () => {
+    startBtnEl.disabled = true
+});
+
+stopBtnEl.addEventListener("click", () => {
+    stopBtnEl.disabled = true
+})
+
+console.log(startBtnEl, stopBtnEl, bodyEl)
