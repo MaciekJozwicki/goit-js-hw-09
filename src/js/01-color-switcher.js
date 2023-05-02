564f5fc18be940ee98387b93bafe0e73276c67f6
Maciek <maciekjozwicki@wp.pl>
2023-05-02 12:47:11 +0200

    task 01 success

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,17 +2,30 @@ const startBtnEl = document.querySelector('button[data-start]')
 const stopBtnEl = document.querySelector('button[data-stop]')
 const bodyEl = document.querySelector('body')
 
+let timer = null;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 
 startBtnEl.addEventListener("click", () => {
+    timer = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+    
+    
     startBtnEl.disabled = true
+    stopBtnEl.disabled = false
+    
 });
 
 stopBtnEl.addEventListener("click", () => {
+    clearTimeout(timer);
+    
     stopBtnEl.disabled = true
+    startBtnEl.disabled = false
 })
 
 console.log(startBtnEl, stopBtnEl, bodyEl)
+
